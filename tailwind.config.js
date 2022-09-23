ccf1113503fe58841d693f8c69640582c60b1616
Serhat KAYA <serhatkaya.sr@gmail.com>
2022-09-24 01:18:21 +0300

    tailwind purge confiugration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	theme: {
 		extend: {},
 	},
 	plugins: [],
 	darkMode: 'class',
+	purge: {
+		content: ['./public/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	},
 };
